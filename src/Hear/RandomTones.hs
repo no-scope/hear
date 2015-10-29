65f04877a24911b56e9abc9f7c8466217ac6e33b
no-scope <vkonton@gmail.com>
2015-10-29 20:22:03 +0200

    Update RandomTones.hs

--- src/Hear/RandomTones.hs
+++ src/Hear/RandomTones.hs
@@ -7,16 +7,18 @@ import           Hear.Music
 randTones :: Int -> (Int, Int) -> [Int] -> IO [Interval]
 randTones n range intervals = go n []
 
-  where go :: Int -> [Interval] -> IO [Interval]
-        go 0 acc = return acc
-        go m acc = do seed <- newStdGen
-                      let xs = takeTwo intervals $ randomRs range seed
-                      go (m-1) (xs:acc)
+  where 
+    go :: Int -> [Interval] -> IO [Interval]
+    go 0 acc = return acc
+    go m acc = do 
+      seed <- newStdGen
+      let xs = takeTwo intervals $ randomRs range seed
+      go (m-1) (xs:acc)
 
-        takeTwo :: [Int]-> [Int]-> Interval
-        takeTwo _      []          = (-1, -1)
-        takeTwo _      [_]         = (-1, -1)
-        takeTwo []     (x : y : _) = (x, y)
-        takeTwo inters (x : y : xs)
-          | abs (x-y) `elem` inters = (x, y)
-          | otherwise               = takeTwo intervals xs
+    takeTwo :: [Int]-> [Int]-> Interval
+    takeTwo _      []          = (-1, -1)
+    takeTwo _      [_]         = (-1, -1)
+    takeTwo []     (x : y : _) = (x, y)
+    takeTwo inters (x : y : xs)
+      | abs (x-y) `elem` inters = (x, y)
+      | otherwise               = takeTwo intervals xs
